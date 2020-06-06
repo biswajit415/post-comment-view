90d68138015856b8388311c365fa7ef4c9952d8e
Biswajit Chatterjee <biswajit@sigaramtech.com>
2020-06-06 19:25:46 +0530

    make it better

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
@@ -18,4 +19,14 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use('/api', require('./routes/userRoute'))
 app.use('/api', require('./routes/postRout'))
+
+
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.listen(port)
