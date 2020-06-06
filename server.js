627ae22002ada8e3f527fa43e1e2e53a3435cf94
Biswajit Chatterjee <biswajit@sigaramtech.com>
2020-06-06 23:47:18 +0530

    updated

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const config = require("./config/key");
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const connect = mongoose.connect(config.mongoURI,
     {
         useNewUrlParser: true, useUnifiedTopology: true,
@@ -20,14 +20,13 @@ app.use(cookieParser());
 app.use('/api', require('./routes/userRoute'))
 app.use('/api', require('./routes/postRout'))
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
 
-// ... other app.use middleware 
-app.use(express.static(path.join(__dirname, "client", "build")))
-
-// ...
-// Right before your app.listen(), add this:
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+    });
+}
+app.listen(PORT, () => {
+    log(`Server is starting at PORT: ${PORT}`);
 });
-console.log(port)
-app.listen(port)
